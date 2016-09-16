ed9821761b821f0a7e5bc556e9259e0b8d4f4887
Megan Shaffer <mshaffer9@gatech.edu>
2016-09-16 13:07:01 -0400

    Revert "Revert "Demo code""
    This reverts commit 10fa57b29b20cf17068237efc7ad01fd3bc5c9de.

--- Assets/Scripts/GrandmaMovement.cs
+++ Assets/Scripts/GrandmaMovement.cs
@@ -4,46 +4,45 @@ using System.Collections.Generic;
 
 public class GrandmaMovement : Movement {
 
-
+    //Keeps track of movement path, to be executed when the level activates
 	private Queue<KeyCode> path;
 
-	// Use this for initialization
 	void Start () {
 		path = new Queue<KeyCode>();
 	}
 	
-	// Update is called once per frame
+    //Queue's grandma's movements with key presses
 	void Update () {
-
 		if(Input.GetKeyDown(KeyCode.W)) {
 			path.Enqueue(KeyCode.W);
 		}
 		if(Input.GetKeyDown(KeyCode.A)) {
 			path.Enqueue(KeyCode.A);
-		}
+        }
 		if(Input.GetKeyDown(KeyCode.S)) {
 			path.Enqueue(KeyCode.S);
-		}
+        }
 		if(Input.GetKeyDown(KeyCode.D)) {
 			path.Enqueue(KeyCode.D);
-		}
+        }
 	}
 
+    //Takes grandma through the movement queue
 	public override void Move ()
 	{
 		if(path.Count > 0) {
 			KeyCode key = path.Dequeue();
 			if(key == KeyCode.W) {
-				transform.Translate(new Vector3(0,2f,0f));
+				transform.Translate(new Vector3(0,1f,0f));
 			}
 			else if(key == KeyCode.A) {
-				transform.Translate(new Vector3(-2f,0f,0f));
+				transform.Translate(new Vector3(-1f,0f,0f));
 			}
 			else if(key == KeyCode.S) {
-				transform.Translate(new Vector3(0,-2f,0f));
+				transform.Translate(new Vector3(0,-1f,0f));
 			}
 			else if(key == KeyCode.D) {
-				transform.Translate(new Vector3(2f,0f,0f));
+				transform.Translate(new Vector3(1f,0f,0f));
 			}
 		}
 	}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Traps!
+//TODO: refactor to handle types, 
+public class Trap : MonoBehaviour {
+
+    public bool active;
+    //Assign type, handle action when called on accordingly
+    public enum TYPE { Sticky, Pushy, Switchy};
+	
+    //Makes trap visible if active
+	public void turnOnIfApplicable() {
+	    if (active)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+	}
+
+    //springs active traps
+    public void spring()
+    {
+        //TODO: handle each type of trap
+    }
+}
