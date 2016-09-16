10fa57b29b20cf17068237efc7ad01fd3bc5c9de
Megan Shaffer <mshaffer9@gatech.edu>
2016-09-16 13:05:49 -0400

    Revert "Demo code"
    This reverts commit c9146278594684c4a556667f695693a611dfd043.

--- Assets/Scripts/GrandmaGhostMove.cs
+++ Assets/Scripts/GrandmaGhostMove.cs
@@ -3,23 +3,24 @@ using System.Collections;
 
 public class GrandmaGhostMove : MonoBehaviour {
 
+	// Use this for initialization
 	void Start () {
 	
 	}
 	
+	// Update is called once per frame
 	void Update () {
-        //Moves with key presses
 		if(Input.GetKeyDown(KeyCode.W)) {
-			transform.Translate(new Vector3(0,1f,0f));
+			transform.Translate(new Vector3(0,2f,0f));
 		}
 		if(Input.GetKeyDown(KeyCode.A)) {
-			transform.Translate(new Vector3(-1f,0f,0f));
+			transform.Translate(new Vector3(-2f,0f,0f));
 		}
 		if(Input.GetKeyDown(KeyCode.S)) {
-			transform.Translate(new Vector3(0,-1f,0f));
+			transform.Translate(new Vector3(0,-2f,0f));
 		}
 		if(Input.GetKeyDown(KeyCode.D)) {
-			transform.Translate(new Vector3(1f,0f,0f));
+			transform.Translate(new Vector3(2f,0f,0f));
 		}
 	}
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,10 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//Parent for different types of object movement
 public class Movement : MonoBehaviour {
-
-    public bool active = true;
 
 	public virtual void Move() {	}
 
